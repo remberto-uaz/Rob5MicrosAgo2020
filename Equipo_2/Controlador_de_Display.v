a0e11512bda660dcc123c8bc030c8a1411e932ff
palomagr16 <71017135+palomagr16@users.noreply.github.com>
2020-09-09 09:00:28 -0500

    Update Controlador_de_Display.v

--- Equipo_2/Controlador_de_Display.v
+++ Equipo_2/Controlador_de_Display.v
@@ -1,17 +1,13 @@
-módulo  Control_Display_7_seg
-    (    entrada i_Clk,
-        entrada i_Rst,
-        entrada [ 3 : 0 ] i_Datos1,
-        entrada [ 3 : 0 ] i_Datos2,
-        entrada [ 3 : 0 ] i_Datos3,
-        entrada [ 3 : 0 ] i_Datos4,
-        salida [ 3 : 0 ] o_Anodo;
-        salida [ 6 : 0 ] o_Segmentos);
+module Control_Display_7_seg
+    (input i_Clk,
+        input i_Rst,
+        input [ 3 : 0 ] i_Datos1,
+        input [ 3 : 0 ] i_Datos2,
+        input [ 3 : 0 ] i_Datos3,
+        input [ 3 : 0 ] i_Datos4,
+        output [ 3 : 0 ] o_Anodo;
+        output [ 6 : 0 ] o_Segmentos);
     
-    alambre [ 3 : 0 ] Demx_Deco_Sel;
-    alambre [ 1 : 0 ] Contador_Demx_Sel;
-    alambre Prescalador_contador_tiempo ;;
-
     Contador_de_anillo  Bloque_1 (
         .i_Clk (i_Clk),
         .i_Rst (i_Rst),
