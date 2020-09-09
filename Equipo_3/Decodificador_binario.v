d94b23201078d78bc3c639cd8adb91fa337c8505
Juan Bernardo <71017199+berna-bot@users.noreply.github.com>
2020-09-09 08:45:36 -0500

    Update Decodificador_binario.v

--- Equipo_3/Decodificador_binario.v
+++ Equipo_3/Decodificador_binario.v
@@ -5,22 +5,22 @@ module Deco_binario_7seg(
 always@(i_bits)
   begin
     case(i_bits)
-      0000: o_segmentos = 0000001;
-      0001: o_segmentos = 1001111:
-      0010: o_segmentos = 0010010;
-      0011: o_segmentos = 0000110;
-      0100: o_segmentos = 1001100;
-      0101: o_segmentos = 0100100;
-      0110: o_segmentos = 0100000;
-      0111: o_segmentos = 0001111;
-      1000: o_segmentos = 0000000;
-      1001: o_segmentos = 0001100;
-      1010: o_segmentos = 0001000;
-      1011: o_segmentos = 0000011;
-      1100: o_segmentos = 1000110;
-      1101: o_segmentos = 0100001;
-      1110: o_segmentos = 0000110;
-      1111: o_segmentos = 0001110;
+      4'b0000: o_segmentos = 7'b0000001;
+      4'b0001: o_segmentos = 1001111:
+      4'b0010: o_segmentos = 0010010;
+      4'b0011: o_segmentos = 0000110;
+      4'b0100: o_segmentos = 1001100;
+      4'b0101: o_segmentos = 0100100;
+      4'b0110: o_segmentos = 0100000;
+      4'b0111: o_segmentos = 0001111;
+      4'b1000: o_segmentos = 0000000;
+      4'b1001: o_segmentos = 0001100;
+      4'b1010: o_segmentos = 0001000;
+      4'b1011: o_segmentos = 0000011;
+      4'b1100: o_segmentos = 1000110;
+      4'b1101: o_segmentos = 0100001;
+      4'b1110: o_segmentos = 0000110;
+      4'b1111: o_segmentos = 0001110;
       default: o_segmentos = 1111111;
     end case;
   end;
