e1cd2367038098cd42c160fd7f7e1ef4e3b73062
leonardob775 <71017506+leonardob775@users.noreply.github.com>
2020-09-09 08:55:01 -0500

    Update Decodificador_binario.v

--- Equipo_3/Decodificador_binario.v
+++ Equipo_3/Decodificador_binario.v
@@ -6,22 +6,22 @@ always@(i_bits)
   begin
     case(i_bits)
       4'b0000: o_segmentos = 7'b0000001;
-      4'b0001: o_segmentos = 1001111:
-      4'b0010: o_segmentos = 0010010;
-      4'b0011: o_segmentos = 0000110;
-      4'b0100: o_segmentos = 1001100;
-      4'b0101: o_segmentos = 0100100;
-      4'b0110: o_segmentos = 0100000;
-      4'b0111: o_segmentos = 0001111;
-      4'b1000: o_segmentos = 0000000;
-      4'b1001: o_segmentos = 0001100;
-      4'b1010: o_segmentos = 0001000;
-      4'b1011: o_segmentos = 0000011;
-      4'b1100: o_segmentos = 1000110;
-      4'b1101: o_segmentos = 0100001;
-      4'b1110: o_segmentos = 0000110;
-      4'b1111: o_segmentos = 0001110;
-      default: o_segmentos = 1111111;
+      4'b0001: o_segmentos = 7'b1001111:
+      4'b0010: o_segmentos = 7'b0010010;
+      4'b0011: o_segmentos = 7'b0000110;
+      4'b0100: o_segmentos = 7'b1001100;
+      4'b0101: o_segmentos = 7'b0100100;
+      4'b0110: o_segmentos = 7'b0100000;
+      4'b0111: o_segmentos = 7'b0001111;
+      4'b1000: o_segmentos = 7'b0000000;
+      4'b1001: o_segmentos = 7'b0001100;
+      4'b1010: o_segmentos = 7'b0001000;
+      4'b1011: o_segmentos = 7'b0000011;
+      4'b1100: o_segmentos = 7'b1000110;
+      4'b1101: o_segmentos = 7'b0100001;
+      4'b1110: o_segmentos = 7'b0000110;
+      4'b1111: o_segmentos = 7'b0001110;
+      default: o_segmentos = 7'b1111111;
     end case;
   end;
 endmodule;
