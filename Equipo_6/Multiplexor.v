535638d1f77ac7e1d4bae1ba282e3485a9958ba2
FranciscoMqz <71017099+FranciscoMqz@users.noreply.github.com>
2020-09-09 08:37:16 -0500

    Update Multiplexor.v

--- Equipo_6/Multiplexor.v
+++ Equipo_6/Multiplexor.v
@@ -7,12 +7,12 @@ module demx
         output [3:0] o_Salida);
 		
 	always@(i_Sel) begin
-        case (i_Sel)
-		2'b00: o_Salida <= i_Datos_0;
-		2'b01: o_Salida <= i_Datos_1;
-		2'b10: o_Salida <= i_Datos_2;
-		default : o_Salida <= i_Datos_3;
-     end case 
-     end 
+		case (i_Sel)
+			2'b00: o_Salida <= i_Datos_0;
+			2'b01: o_Salida <= i_Datos_1;
+			2'b10: o_Salida <= i_Datos_2;
+			default : o_Salida <= i_Datos_3;
+		endcase 
+     	end 
     
 endmodule 
