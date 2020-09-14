3fd2ac5cb779d9858bcef40df53ee827aa079ed1
JesusMartinezQ6820 <71016992+JesusMartinezQ6820@users.noreply.github.com>
2020-09-14 09:30:46 -0500

    Update Prescalador.v

--- Equipo_4/Prescalador.v
+++ Equipo_4/Prescalador.v
@@ -2,26 +2,21 @@ module Prescalador(
   input i_clock,
   input i_reset,
   output o_presc
-);
-
+  );
   reg [19:0] contador;
   reg salida;
   always@(posedge i_clock)
- begin
-
- if (i_reset)
-   contador <= 0;
- else
-   contador <= contador+1;
-
- if (contador <= 416666)
-   salida <= 0;
- else
-   salida <= 1;
-
- if (contador == 833333)
-   contador <= 0;
-
+  begin
+      if (i_reset)
+      contador <= 0;
+      else
+      contador <= contador+1;
+      if (contador <= 416667)
+      salida <= 0;
+      else
+      salida <= 1;
+      if (contador == 833334)
+      contador <= 0;
   end
- assign o_presc = salida;
+  assign o_presc = salida;
 endmodule
