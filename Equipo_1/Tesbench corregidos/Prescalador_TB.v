23675e82b04ed63062e855920fc50e1118d5876e
JAVIER-LOPEZ-VAZQUEZ <71017771+JAVIER-LOPEZ-VAZQUEZ@users.noreply.github.com>
2020-09-15 10:56:09 -0500

    Add files via upload

---
+++ Equipo_1/Tesbench corregidos/Prescalador_TB.v
@@ -0,0 +1,23 @@
+module Prescalador_TB(
+);
+    reg clk;
+    reg reset;
+    wire clk_out;
+
+    Prescalador DUT (
+        .i_Clk(clk),
+        .i_Rst(reset),
+        .o_Presc(clk_out)
+    );
+
+    initial begin
+        clk<=0;
+        reset<=1;
+        #20 reset<=0;
+    end
+
+    always@(clk) begin
+        #5 clk <= ~clk;
+    end
+
+endmodule
