f4d7177c65c42d58c55fa49c529d5d0bd42cbf90
Paco Ruiz Bravo <71016876+PacoRB29@users.noreply.github.com>
2020-09-14 21:47:12 -0500

    Update decodificador_binario_7seg.v

--- Equipo_1/Controlador_Display_Equipo_1/decodificador_binario_7seg.v
+++ Equipo_1/Controlador_Display_Equipo_1/decodificador_binario_7seg.v
@@ -7,52 +7,52 @@ module decodificador_binario_7seg
     @always(i_Entrada)begin
         case (i_Entrada) 
             4'b0000: begin
-                o_Salida<=7b'0000001;
+                o_Salida<=7'b0000001;
                 end
             4'b0001: begin
-                o_Salida<=7b'1001111;
+                o_Salida<=7'b1001111;
                 end
             4'b0010: begin
-                o_Salida<=7b'0010010;
+                o_Salida<=7'b0010010;
                 end
             4'b0011: begin
-                o_Salida<=7b'0000110;
+                o_Salida<=7'b0000110;
                 end
             4'b0100: begin
-                o_Salida<=7b'1001100;
+                o_Salida<=7'b1001100;
                 end
             4'b0101: begin
-                o_Salida<=7b'0100100;
+                o_Salida<=7'b0100100;
                 end
             4'b0110: begin
-                o_Salida<=7b'0100000;
+                o_Salida<=7'b0100000;
                 end
             4'b0111: begin
-                o_Salida<=7b'0001111;
+                o_Salida<=7'b0001111;
                 end
             4'b1000: begin
-                o_Salida<=7b'0000000;
+                o_Salida<=7'b0000000;
                 end
             4'b1001: begin
-                o_Salida<=7b'0001100;
+                o_Salida<=7'b0001100;
                 end
             4'b1010: begin
-                o_Salida<=7b'0001000;
+                o_Salida<=7'b0001000;
                 end
             4'b1011: begin
-                o_Salida<=7b'1100000;
+                o_Salida<=7'b1100000;
                 end
             4'b1100: begin
-                o_Salida<=7b'0110001;
+                o_Salida<=7'b0110001;
                 end
             4'b1101: begin
-                o_Salida<=7b'1000010;
+                o_Salida<=7'b1000010;
                 end
             4'b1110: begin
-                o_Salida<=7b'0110000;
+                o_Salida<=7'b0110000;
                 end
             default: begin
-                o_Salida<=7b'0111000;
+                o_Salida<=7'b0111000;
                  end
         endcase
     end
