6381c4352df5ea509dbb393a801e50ba27a81859
Ana Martínez <71017041+somtz@users.noreply.github.com>
2020-09-15 13:34:30 -0500

    Add files via upload

---
+++ Equipo_1/Tesbench corregidos/Decodificador_binario_7_seg_TB.v
@@ -0,0 +1,31 @@
+module Deco_binario_7seg_TB(
+);
+ reg  [3:0] bits;
+ wire [6:0] seg;
+
+Deco_binario_7seg DUT(
+    .i_Bits(bits),
+    .o_Segmentos(seg)
+);
+
+initial
+ begin
+    bits<=4'b0000; #50
+	bits<=4'b0001; #50
+    bits<=4'b0010; #50
+    bits<=4'b0011; #50
+    bits<=4'b0100; #50
+    bits<=4'b0101; #50
+    bits<=4'b0110; #50
+    bits<=4'b0111; #50
+    bits<=4'b1000; #50
+    bits<=4'b1001; #50
+	bits<=4'b1010; #50
+    bits<=4'b1011; #50
+    bits<=4'b1100; #50
+    bits<=4'b1101; #50
+    bits<=4'b1110; #50
+    bits<=4'b1111;
+    end
+
+endmodule
