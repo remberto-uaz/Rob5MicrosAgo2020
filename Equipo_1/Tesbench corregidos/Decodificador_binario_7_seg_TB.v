98d8bf1b41ca761fa996cfaa65815d01b4056f89
Paco Ruiz Bravo <71016876+PacoRB29@users.noreply.github.com>
2020-09-16 19:17:38 -0500

    Update Decodificador_binario_7_seg_TB.v

--- Equipo_1/Tesbench corregidos/Decodificador_binario_7_seg_TB.v
+++ Equipo_1/Tesbench corregidos/Decodificador_binario_7_seg_TB.v
@@ -1,31 +1,46 @@
 module Deco_binario_7seg_TB(
 );
- reg  [3:0] bits;
- wire [6:0] seg;
+    reg  [3:0] bits;
+    wire [6:0] seg;
 
-Deco_binario_7seg DUT(
-    .i_Bits(bits),
-    .o_Segmentos(seg)
-);
+    Deco_binario_7seg DUT(
+        .i_Bits(bits),
+        .o_Segmentos(seg)
+    );
 
-initial
- begin
-    bits<=4'b0000; #50
-	bits<=4'b0001; #50
-    bits<=4'b0010; #50
-    bits<=4'b0011; #50
-    bits<=4'b0100; #50
-    bits<=4'b0101; #50
-    bits<=4'b0110; #50
-    bits<=4'b0111; #50
-    bits<=4'b1000; #50
-    bits<=4'b1001; #50
-	bits<=4'b1010; #50
-    bits<=4'b1011; #50
-    bits<=4'b1100; #50
-    bits<=4'b1101; #50
-    bits<=4'b1110; #50
-    bits<=4'b1111;
+    initial
+    begin
+        bits<=4'b0000;
     end
-
+    always@(bits)begin
+        bits<=4'b0001;
+        #5
+        bits<=4'b0010;
+        #5
+        bits<=4'b0011;
+        #5
+        bits<=4'b0100;
+        #5
+        bits<=4'b0101;
+        #5
+        bits<=4'b0110;
+        #5
+        bits<=4'b0111;
+        #5
+        bits<=4'b1000;
+        #5
+        bits<=4'b1001;
+        #5
+        bits<=4'b1010;
+        #5
+        bits<=4'b1011;
+        #5
+        bits<=4'b1100;
+        #5
+        bits<=4'b1101;
+        #5
+        bits<=4'b1110;
+        #5
+        bits<=4'b1111;
+    end
 endmodule
