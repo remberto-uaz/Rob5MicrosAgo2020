957cde61530268e18674a120246a299430373fdf
JesusMartinezQ6820 <71016992+JesusMartinezQ6820@users.noreply.github.com>
2020-09-16 20:56:15 -0500

    Add files via upload

---
+++ Equipo_4/Testbench/Contador_de_anillo_TB.v
@@ -0,0 +1,24 @@
+module Contador_de_anillo_TB(
+);
+    reg clk;
+    reg rst;
+    wire [3:0] anodo;
+    wire [1:0] sel;
+    Contador_de_anillo DUT (
+        .i_Clk(clk),
+        .i_Rst(rst),
+        .o_Anodo(anodo),
+        .o_Sel(sel)
+    );
+    initial 
+    begin
+        clk<=0;
+        rst<=1;
+        #20 
+        rst<=0;
+    end
+    always@(clk) 
+	begin
+		#5 clk <= ~clk;
+    end
+endmodule
