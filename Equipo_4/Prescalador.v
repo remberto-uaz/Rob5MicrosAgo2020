9c24970d5f428af06db3f7d719296a77f0cab8e9
HumbertoHz <71017175+HumbertoHz@users.noreply.github.com>
2020-09-16 20:49:47 -0500

    Update Prescalador.v

--- Equipo_4/Prescalador.v
+++ Equipo_4/Prescalador.v
@@ -8,15 +8,15 @@ module Preescalador(
   always@(posedge i_Clk)
   begin
       if (i_Rst)
-      contador <= 0;
+          contador <= 0;
       else
-      contador <= contador+1;
+          contador <= contador+1;
       if (contador <= 416666)
-      salida <= 0;
+          salida <= 0;
       else
-      salida <= 1;
+          salida <= 1;
       if (contador == 833333)
-      contador <= 0;
+          contador <= 0;
   end
   assign o_Presc = salida;
 endmodule
