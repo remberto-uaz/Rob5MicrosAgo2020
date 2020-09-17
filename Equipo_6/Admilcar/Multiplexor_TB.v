590c4ae0761d7d540801090197344d8c3051e02a
Admilcar <71017649+Admilcar@users.noreply.github.com>
2020-09-17 17:50:06 -0500

    Update Multiplexor_TB.v

--- Equipo_6/Admilcar/Multiplexor_TB.v
+++ Equipo_6/Admilcar/Multiplexor_TB.v
@@ -1,1 +1,51 @@
+  
+//`timescale 1ns / 1ps
 
+
+module Multiplexor_TB(
+
+    );
+    
+ reg [3:0] Datos_0;   
+ reg [3:0] Datos_1;
+ reg [3:0] Datos_2;
+ reg [3:0] Datos_3;
+ reg [1:0] Sel;
+ wire [3:0] Salida;
+ 
+ Multiplexor DUT(
+     .i_Datos_0(Datos_0),  
+     .i_Datos_1(Datos_1),
+     .i_Datos_2(Datos_2),
+     .i_Datos_3(Datos_3),
+     .i_Sel(Sel),
+     .o_Salida(Salida)
+ );
+ 
+ initial
+begin
+  
+  Datos_0 <=0;
+  Datos_1 <=0;
+  Datos_2 <=0;
+  Datos_3 <=0;
+  Sel <=0;
+  #50;
+  Datos_0 [0]<=1;
+  Datos_1 [1]<=1;
+  Datos_2 [2]<=1;
+  Datos_3 [3]<=1;
+  #50;
+  Sel <=0;
+  #50;
+  Sel [0]<=1;
+  Sel [1]<=0;
+  #50;
+  Sel [0]<=0;
+  Sel [1]<=1;
+  #50;
+  Sel [0]<=1;
+  Sel [1]<=1;
+  #50;
+end
+endmodule
