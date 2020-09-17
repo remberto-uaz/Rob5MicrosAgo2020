4f57feea64511fa51b69b068daf69a5b080e2250
JesusMartinezQ6820 <71016992+JesusMartinezQ6820@users.noreply.github.com>
2020-09-16 20:11:01 -0500

    Add files via upload

---
+++ Equipo_4/Testbench/Contador_de_anillo_TB.v
@@ -0,0 +1,27 @@
+module Contador_de_anillo_TB(
+);
+    reg clk;
+    reg rst;
+    wire [3:0] anodo;
+    wire [1:0] sel;
+
+    Contador_de_anillo DUT (
+        .i_Clk(clk),
+        .i_Rst(rst),
+        .o_Anodo(anodo),
+        .o_Sel(sel)
+    );
+
+    initial 
+    begin
+        clk<=0;
+        rst<=1;
+        #20 
+        rst<=0;
+    end
+
+    always@(clk) begin
+        #416667 clk <= ~clk;
+    end
+
+endmodule
---
+++ Equipo_4/Testbench/Preescalador_TB.v
@@ -0,0 +1,19 @@
+module Preescalador_TB(
+    );
+    reg clk;
+    reg reset;
+    wire clk_out;
+    Preescalador DUT (
+      .i_Clk(clk),
+      .i_Rst(reset),
+      .o_Presc(clk_out)
+    );
+    initial
+    begin
+      clk<=0;
+      reset<=1;
+      #20 reset<=0;
+    end
+    always@(clk)
+      #5 clk <= ~clk;
+endmodule
