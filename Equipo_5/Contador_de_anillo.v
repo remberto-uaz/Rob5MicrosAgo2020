2f6c58a2f51d1a1258f20aac934b83cf9d392d28
Jonathan Garcia <71016793+Shoneko@users.noreply.github.com>
2020-09-16 20:16:50 -0500

    Update Contador_de_anillo.v

--- Equipo_5/Contador_de_anillo.v
+++ Equipo_5/Contador_de_anillo.v
@@ -3,22 +3,18 @@ module Contador_de_anillo(
     input i_Rst,
     output reg [3:0] o_Anodo,
     output reg [1:0] o_Sel);
-
-    reg [1:0] cont;
-
+    
  always @(posedge i_Rst, posedge i_Clk)
     begin
         if(i_Rst) begin
             o_Anodo<= 4'b0001;
             o_Sel<=2'b00;   
-            cont<=2'b00;
         end
         else begin
-            case(cont)
+            case(o_Sel)
                 2'b00: begin
                     o_Anodo<= 4'b0001;
-                    o_Sel<=cont;  
-                    cont<=cont+1;
+                    o_Sel<=o_Sel+1;
                 end
                 2'b01: begin
                     o_Anodo<= 4'b0010;
