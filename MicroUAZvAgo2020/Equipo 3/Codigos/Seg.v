ca6c97b8635c049b3593f76adb1fa8c55a6dc3d6
Ricardo-H-D <71022266+Ricardo-H-D@users.noreply.github.com>
2020-11-17 21:53:08 -0600

    Update Seg.v

--- MicroUAZvAgo2020/Equipo 3/Codigos/Seg.v
+++ MicroUAZvAgo2020/Equipo 3/Codigos/Seg.v
@@ -1,39 +1,13 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: UAZ
-// Engineer: Juan Bernardo Esparza Luevano
-// 
-// Create Date: 03.11.2020 13:48:33
-// Design Name: 
-// Module Name: Seg
-// Project Name: Microprocesador
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Seg(
     input [8:0] Instruction,
-    output reg [2:0] Inst,
-    output reg [2:0] RX,
-    output reg [2:0] RY
+    output [2:0] Inst,
+    output [2:0] RX,
+    output [2:0] RY
     );
-  
-  always@(Instruction)
-  begin
-  
 
-  Inst <= Instruction[8:6];
-  RX <= Instruction[5:3];
-  RY <= Instruction[2:0];
-    
-  end
+    assign Inst = Instruction[8:6];
+    assign RX = Instruction[5:3];
+    assign RY = Instruction[2:0];
 endmodule
