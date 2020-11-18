d2779245f09162b376ea12fc6c5ebec31233b536
Ricardo-H-D <71022266+Ricardo-H-D@users.noreply.github.com>
2020-11-17 21:45:28 -0600

    Update Flags.v

--- MicroUAZvAgo2020/Equipo 3/Codigos/Flags.v
+++ MicroUAZvAgo2020/Equipo 3/Codigos/Flags.v
@@ -4,15 +4,15 @@ module Flags(
     input [2:0] FLAG_IN,
     input clk,
     input reset,
-    input [1:0] HAB,
+    input [2:0] HAB,
     output reg [2:0] FLAG_OUT
     );
-    always@(posedge clk, posedge reset)
-        begin
-            if (reset == 1)
-                FLAG_OUT <= 3'b000;
-            else
-                if(HAB == 01)
-                    FLAG_OUT <= FLAG_IN;
-        end
+always@(posedge clk, posedge reset)
+    begin
+        if (reset == 1)
+            FLAG_OUT <= 3'b000;
+        else
+            if(HAB == 001)
+                FLAG_OUT <= FLAG_IN;
+    end
 endmodule
