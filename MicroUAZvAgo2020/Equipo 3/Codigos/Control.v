b8dc396d2033044bff99b3e556b7556c3f6bec8b
Ricardo-H-D <71022266+Ricardo-H-D@users.noreply.github.com>
2020-11-17 21:44:40 -0600

    Update Control.v

--- MicroUAZvAgo2020/Equipo 3/Codigos/Control.v
+++ MicroUAZvAgo2020/Equipo 3/Codigos/Control.v
@@ -1,119 +1,92 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: UAZ
-// Engineer: Juan Bernardo Esparza Luevano
-// 
-// Create Date: 08.11.2020 13:22:25
-// Design Name: 
-// Module Name: Control
-// Project Name: Microprocesador
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Control(
     input [2:0] Inst,
     input [1:0] COND,
-    output reg SEL_R,
+    output reg RW,
     output reg [2:0] SELEC,
     output reg DONE,
-    output reg [1:0] HAB
+    output reg [2:0] HAB
     );
     
-    always @(Inst, COND, SEL_R, SELEC, DONE, HAB)
+    always @*
     begin
         case(Inst)
-        3'b001:
-        begin
-            SEL_R <= 0;
-            SELEC <= 001;
-            DONE <= 1;
-            HAB <= 00;
-        end
-       
-        3'b010:
-        begin
-            SEL_R <= 1;
-            SELEC <= 011;
-            DONE <= 1;
-            HAB <= 00;
-        end
-        
-        3'b011:
-        begin
-            SEL_R <= 0;
-            SELEC <= 101;
-            DONE <= 1;
-            HAB <= 00;
-        end
-        
-        3'b100:
-        begin
-            SEL_R <= 0;
-            SELEC <= 110;
-            DONE <= 1;
-            HAB <= 00;
-        end
-        
-        3'b101:
-        begin
-            SEL_R <= 0;
-            SELEC <= 010;
-            DONE <= 1;
-            HAB <= 00;
-        end
-        
-        3'b110:
-        begin
-            SEL_R <= 0;
-            SELEC <= 000;
-            DONE <= 1;
-            HAB <= 01;
-        end
-        
-        3'b111:
-        begin
-            case(COND)
-                2'b01:
+            3'b001:
                 begin
-                    SEL_R <= 0;
+                    RW <= 1'bz;
+                    SELEC <= 001;
+                    DONE <= 1;
+                    HAB <= 100;
+                end
+            3'b010:
+                begin
+                    RW <= 0;
+                    SELEC <= 011;
+                    DONE <= 1;
+                    HAB <= 100;
+                end
+            3'b011:
+                begin
+                    RW <= 1;
+                    SELEC <= 101;
+                    DONE <= 1;
+                    HAB <= 000;
+                end
+            3'b100:
+                begin
+                    RW <= 1;
+                    SELEC <= 110;
+                    DONE <= 1;
+                    HAB <= 000;
+                end
+            3'b101:
+                begin
+                    RW <= 1'bz;
+                    SELEC <= 010;
+                    DONE <= 1;
+                    HAB <= 100;
+                end
+            3'b110:
+                begin
+                    RW <= 1'bz;
                     SELEC <= 000;
                     DONE <= 1;
-                    HAB <= 10;
+                    HAB <= 001;
                 end
-                2'b11:
+            3'b111:
                 begin
-                    SEL_R <= 0;
-                    SELEC <= 000;
-                    DONE <= 1;
-                    HAB <= 11;
+                    case(COND)
+                        2'b01:
+                        begin
+                            RW <= 1'bz;
+                            SELEC <= 000;
+                            DONE <= 1;
+                            HAB <= 010;
+                        end
+                        2'b11:
+                        begin
+                            RW <= 1'bz;
+                            SELEC <= 000;
+                            DONE <= 1;
+                            HAB <= 011;
+                        end
+                        default:
+                        begin
+                            RW <= 1'bz;
+                            SELEC <= 000;
+                            DONE <= 0;
+                            HAB <= 000;
+                        end
+                    endcase
                 end
-                default:
+            default:
                 begin
-                    SEL_R <= 0;
-                    SELEC <= 000;
-                    DONE <= 0;
-                    HAB <= 00;
+                    RW <= 1'bz;
+                    SELEC <= 3'b000;
+                    DONE <= 1'b0;
+                    HAB <= 3'b000;
                 end
-            endcase
-        end
-        
-        default:
-        begin
-            SEL_R <= 0;
-            SELEC <= 000;
-            DONE <= 0;
-            HAB <= 00;
-        end
         endcase
     end
 endmodule
