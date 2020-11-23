bf7119a7998443328040b0a7317fc5ef7eb04c3c
Ricardo-H-D <71022266+Ricardo-H-D@users.noreply.github.com>
2020-11-23 05:45:19 -0600

    Update PC.v

--- MicroUAZvAgo2020/Equipo 3/Codigos/PC.v
+++ MicroUAZvAgo2020/Equipo 3/Codigos/PC.v
@@ -6,19 +6,21 @@ module PC(
     input DONE,
     input [7:0] RX_DATO,
     input [2:0] HAB,
-    output reg[7:0] PC_VAL
+    output reg [7:0] PC_VAL
     );
-
-always@(posedge clk)
+    
+always@(posedge clk, posedge reset)
     begin
-        if(reset == 1'b1)
+        if(reset == 1)
                 PC_VAL <= 8'b00000000;
         else  
+           begin
            case(HAB)
                 3'b010: PC_VAL<=RX_DATO;
                 3'b011: PC_VAL<=RX_DATO;
-            endcase
+           endcase
            if(DONE == 1'b1)
                 PC_VAL=PC_VAL+1;
            end
+       end
 endmodule
