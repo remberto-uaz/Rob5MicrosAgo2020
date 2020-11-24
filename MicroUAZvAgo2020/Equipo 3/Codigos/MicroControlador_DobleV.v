7dd74ce53752dc0ada605e3b79de602b7dabc0f6
Ricardo-H-D <71022266+Ricardo-H-D@users.noreply.github.com>
2020-11-23 22:27:46 -0600

    Update MicroControlador_DobleV.v

--- MicroUAZvAgo2020/Equipo 3/Codigos/MicroControlador_DobleV.v
+++ MicroUAZvAgo2020/Equipo 3/Codigos/MicroControlador_DobleV.v
@@ -1,8 +1,10 @@
 `timescale 1ns / 1ps
 
 module MicroControlador_DobleV(
-    input clk,
-    input reset
+        input clk,
+        input reset,
+        output [7:0] DATA_OUT,
+        output [7:0] DIR_OUT
     );
     
     wire [8:0] INS;
@@ -11,6 +13,8 @@ module MicroControlador_DobleV(
     wire [7:0] AD_Bus;
     wire [7:0] DO_Bus;
     wire R_W;
+assign DATA_OUT = DI_Bus;
+assign DIR_OUT = AD_Bus;
 
 Microprocesador_DobleV Micro(
     .clk(clk),
