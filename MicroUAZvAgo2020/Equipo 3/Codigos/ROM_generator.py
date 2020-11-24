01aa818dbc44e9fbae731a94c4ebf1b77ac8cc00
Ricardo-H-D <71022266+Ricardo-H-D@users.noreply.github.com>
2020-11-23 22:47:28 -0600

    Update ROM_generator.py

--- MicroUAZvAgo2020/Equipo 3/Codigos/ROM_generator.py
+++ MicroUAZvAgo2020/Equipo 3/Codigos/ROM_generator.py
@@ -1,17 +1,4 @@
 file = open("D:/MicroUAZ_DobleV/MicroUAZ_DobleV.srcs/sources_1/new/ROM.mem", "w")
-#si quiero multiplicar 3 por 4
-"""file.write("001001011" + '\n')
-
-file.write("110001000" + '\n')
-file.write("110001000" + '\n')
-file.write("110001000" + '\n')
-file.write("110001000" + '\n')
-
-file.write("100010000" + '\n')
-
-file.write("000000000" + '\n')
-file.close()"""
-
 
 x = int(input("Introduce el primer operando: "))
 y = int(input("Introduce el segundo operando: "))
